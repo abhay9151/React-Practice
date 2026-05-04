f757dd2472f0348a7a0bc700f5b3ce3ee58963f1
Abhay Pratap Singh <saurabhsingh99195@gmail.com>
2026-05-04 15:48:18 +0530

    Iearned props

---
+++ @latest/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Card from './components/card';
+
+const App = () => {
+  return (
+    <div className="Container">
+
+      <Card name="Love Babbar"  desc="desc1" />
+      <Card name="Love Babbar" desc="desc2" />
+      <Card name="Love Babbar" desc="desc3" />
+      <Card name="Love Babbar" desc="desc4" />
+
+
+    </div>
+  );
+};
+
+export default App;
---
+++ @latest/src/components/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import lovepic from '../assets/hero.png';
+import './card.css';
+
+const Card = (props) => {
+  return (
+    <div className='container'>
+      <p id='user-name'>{props.name}</p>
+    <img id='user-img' src={lovepic} alt='love' />
+    <p id='user-desc'>{props.desc}</p>
+    </div>
+  );
+};
+
+export default Card;
